f924a6598a87bdda6ecdaac54495e80bd6cfff50
winex01 <winnie131212592@gmail.com>
2019-01-09 18:50:52 +0800

    WIP: index page

---
+++ resources/views/layouts/main-nav.blade.php
@@ -0,0 +1,27 @@
+  <!-- Left side column. contains the sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+
+        <li class="active">
+            <a href="https://adminlte.io/docs"><i class="fa fa-home"></i> <span>Home</span></a>
+        </li>
+
+        <li>
+            <a href="https://adminlte.io/docs"><i class="fa fa-fire"></i> <span>Trending</span></a>
+        </li>
+
+        <li>
+            <a href="https://adminlte.io/docs"><i class="fa fa-upload"></i> <span>Submit Code</span></a>
+        </li>
+
+        <li class="header">PROGRAMMING</li>
+        <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
---
+++ resources/views/layouts/searchbar.blade.php
@@ -0,0 +1,18 @@
+<section class="content-header">
+  <div class="row">
+    <div class="col-md-6">
+        <!-- search form -->
+        <form action="#" method="get">
+          <div class="input-group">
+            <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                  <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                  </button>
+                </span>
+          </div>
+        </form>
+        <!-- /.search form -->      
+    </div>
+    <div class="col-md-6"></div>
+  </div>
+</section>
