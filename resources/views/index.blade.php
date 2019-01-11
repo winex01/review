19a5bd21f474ce13b41994a7727666af13277961
winex01 <winnie131212592@gmail.com>
2019-01-11 19:07:44 +0800

    WIP: index page project file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,86 @@
 
 @section('content')
 
-  Start creating your amazing application!
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_1.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>School Management System</strong>
+			<br/>
+			<a href="#">JohnDoe</a>
+			<br>
+			85K views • 1 week ago
+		</div>
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_2.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>Points of Sales</strong>
+			<br/>
+			<a href="#">Renegade</a>
+			<br>
+			2.5M views • 2 years ago
+		</div>
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_3.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>PHP sample ajax request</strong>
+			<br/>
+			<a href="#">DarkMage</a>
+			<br>
+			21K views • 5 days ago
+		</div>
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_4.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>Laravel Multiple User Roles</strong>
+			<br/>
+			<a href="#">Namuro Kun Sage</a>
+			<br>
+			250K views • 1 month ago
+		</div>
+	</div>
 
+<hr>
+
+	<div class="row">
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_5.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>Code Igniter Sample Login System</strong>
+			<br/>
+			<a href="#">RiverCzz</a>
+			<br>
+			889K views • 3 weeks ago
+		</div>
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_6.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>Hotel Management System</strong>
+			<br/>
+			<a href="#">Lorszi</a>
+			<br>
+			5M views • 4 years ago
+		</div>
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_7.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>C# how to fix this Bug</strong>
+			<br/>
+			<a href="#">YourSSiYol</a>
+			<br>
+			3K views • 3 days ago
+		</div>
+		<div class="col-md-3">
+			<img src="{{ url('images/Screenshot_8.png') }}" class="img-thumbnail" alt="Responsive image">
+			<strong>Online enrollment system</strong>
+			<br/>
+			<a href="#">Tempest</a>
+			<br>
+			903K views • 11 months ago
+		</div>
+	</div>
+</div>
+<ul class="pagination">
+	<li><a href="#">&laquo;</a></li>
+	<li><a href="#">1</a></li>
+	<li><a href="#">2</a></li>
+	<li><a href="#">3</a></li>
+	<li><a href="#">4</a></li>
+	<li><a href="#">5</a></li>
+	<li><a href="#">&raquo;</a></li>
+</ul>
 @endsection
