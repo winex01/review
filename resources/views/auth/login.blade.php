e522ce0dff4f07b943dd47af8acbb306e93fecf8
winex01 <winnie131212592@gmail.com>
2019-01-12 20:53:57 +0800

    add route name in login facebook route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
 
     <div class="social-auth-links text-center">
       <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+      <a href="{{ route('login.facebook') }}" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
         Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
         Google+</a>
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('login/facebook', 'Auth\LoginController@redirectToProvider');
+Route::get('login/facebook', 'Auth\LoginController@redirectToProvider')->name('login.facebook');
 Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
