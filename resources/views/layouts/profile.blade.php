f849b0bd010a5b1cf91e64a491bf47312ade8fbf
winex01 <winnie131212592@gmail.com>
2019-01-12 21:19:17 +0800

    profile avatar and credentials

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -83,17 +83,17 @@
   <!-- User Account: style can be found in dropdown.less -->
   <li class="dropdown user user-menu">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-      <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-      <span class="hidden-xs">John Pierce</span>
+      <img src="{{ auth()->user()->avatar }}" class="user-image" alt="User Image">
+      <span class="hidden-xs">{{ auth()->user()->name }}</span>
     </a>
     <ul class="dropdown-menu">
       <!-- User image -->
       <li class="user-header">
-        <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        <img src="{{ auth()->user()->avatar }}" class="img-circle" alt="User Image">
 
         <p>
-          Alexander Pierce - Web Developer
-          <small>Member since Nov. 2012</small>
+          {{ auth()->user()->name }}
+          <small>Member since {{ auth()->user()->created_at->format('M. d, Y') }}</small>
         </p>
       </li>
       <!-- Menu Body -->
