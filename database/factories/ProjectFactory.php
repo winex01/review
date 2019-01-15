bb6adca819f759116c4257c1af54e5d9c8f45416
winex01 <winnie131212592@gmail.com>
2019-01-15 11:32:40 +0800

    project factory description generate 1000 text

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Project::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(5),
-        'description' => $faker->text(),
+        'description' => $faker->realText(1000),
     ];
 });
